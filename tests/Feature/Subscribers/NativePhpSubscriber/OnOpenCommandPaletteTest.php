46aaef9172a48738734c220305a1690dc5bbe015
XbNz <XbNz@users.noreply.github.com>
2024-11-13 00:17:59 +0000

    Fix styling

--- tests/Feature/Subscribers/NativePhpSubscriber/OnOpenCommandPaletteTest.php
+++ tests/Feature/Subscribers/NativePhpSubscriber/OnOpenCommandPaletteTest.php
@@ -1,16 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Feature\Subscribers\NativePhpSubscriber;
 
 use App\Events\OpenCommandPaletteEvent;
-use App\Subscribers\NativePhpSubscriber;
 use Illuminate\Contracts\Events\Dispatcher;
 use Native\Laravel\Facades\Window;
 use Native\Laravel\Windows\Window as WindowClass;
 use Tests\TestCase;
 use XbNz\Shared\Enums\NativePhpWindow;
 
-class OnOpenCommandPaletteTest extends TestCase
+final class OnOpenCommandPaletteTest extends TestCase
 {
     #[\PHPUnit\Framework\Attributes\Test]
     public function it_opens_the_search_command_palette(): void
@@ -22,7 +23,7 @@ class OnOpenCommandPaletteTest extends TestCase
             ->andReturn($fakeWindow);
 
         // Act
-        $this->app->make(Dispatcher::class)->dispatch(new OpenCommandPaletteEvent);
+        $this->app->make(Dispatcher::class)->dispatch(new OpenCommandPaletteEvent());
 
         // Assert
         $this->assertSame(route('search'), invade($fakeWindow)->url);
