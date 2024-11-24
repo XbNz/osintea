a88d690f0159f912fdb990a9313013154c3ab514
A G <fdwnct@gmail.com>
2024-11-24 03:20:39 -0500

    masscan and port modules

---
+++ app-modules/port/routes/port-routes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// use XbNz\Port\Http\Controllers\PortController;
+
+// Route::get('/ports', [PortController::class, 'index'])->name('ports.index');
+// Route::get('/ports/create', [PortController::class, 'create'])->name('ports.create');
+// Route::post('/ports', [PortController::class, 'store'])->name('ports.store');
+// Route::get('/ports/{port}', [PortController::class, 'show'])->name('ports.show');
+// Route::get('/ports/{port}/edit', [PortController::class, 'edit'])->name('ports.edit');
+// Route::put('/ports/{port}', [PortController::class, 'update'])->name('ports.update');
+// Route::delete('/ports/{port}', [PortController::class, 'destroy'])->name('ports.destroy');
---
+++ app-modules/port/src/Providers/PortServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XbNz\Port\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+final class PortServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
