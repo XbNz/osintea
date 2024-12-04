4c2451f172e56d54210bb4b3ee99c51372530d1c
A G <fdwnct@gmail.com>
2024-12-03 21:48:58 -0500

    remove scrollbar

--- resources/views/components/layouts/secondary-window.blade.php
+++ resources/views/components/layouts/secondary-window.blade.php
@@ -11,6 +11,13 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
+    <style>
+        ::-webkit-scrollbar {
+            width: 0px;
+            height: 12px;
+        }
+    </style>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
     @fluxStyles
