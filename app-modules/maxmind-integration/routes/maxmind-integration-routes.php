76174b60f253c81cf63590acf5036f5077f7c963
A G <fdwnct@gmail.com>
2024-12-11 02:00:15 -0500

    maxmind download workflows

---
+++ app-modules/maxmind-integration/routes/maxmind-integration-routes.php
@@ -0,0 +1,11 @@
+<?php
+
+// use XbNz\MaxmindIntegration\Http\Controllers\MaxmindIntegrationController;
+
+// Route::get('/maxmind-integrations', [MaxmindIntegrationController::class, 'index'])->name('maxmind-integrations.index');
+// Route::get('/maxmind-integrations/create', [MaxmindIntegrationController::class, 'create'])->name('maxmind-integrations.create');
+// Route::post('/maxmind-integrations', [MaxmindIntegrationController::class, 'store'])->name('maxmind-integrations.store');
+// Route::get('/maxmind-integrations/{maxmind-integration}', [MaxmindIntegrationController::class, 'show'])->name('maxmind-integrations.show');
+// Route::get('/maxmind-integrations/{maxmind-integration}/edit', [MaxmindIntegrationController::class, 'edit'])->name('maxmind-integrations.edit');
+// Route::put('/maxmind-integrations/{maxmind-integration}', [MaxmindIntegrationController::class, 'update'])->name('maxmind-integrations.update');
+// Route::delete('/maxmind-integrations/{maxmind-integration}', [MaxmindIntegrationController::class, 'destroy'])->name('maxmind-integrations.destroy');
