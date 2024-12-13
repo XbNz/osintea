ccc6813df1a0434d95b8f125d42507c35cfb19d6
A G <fdwnct@gmail.com>
2024-12-13 04:29:39 -0500

    update deps, add dark mode toggle in preferences

--- resources/views/livewire/preferences.blade.php
+++ resources/views/livewire/preferences.blade.php
@@ -1,10 +1,31 @@
 <div>
     <flux:tab.group>
-        <flux:tabs wire:model="tab">
-            <flux:tab name="fping">Fping</flux:tab>
-            <flux:tab name="masscan">Masscan</flux:tab>
-        </flux:tabs>
-
+        <div class="flex items-center justify-between">
+            <flux:tabs wire:model="tab">
+                <flux:tab name="fping">Fping</flux:tab>
+                <flux:tab name="masscan">Masscan</flux:tab>
+            </flux:tabs>
+            <flux:radio.group variant="segmented" x-model="$flux.appearance">
+                <flux:radio value="light">
+                    <div class="flex items-center">
+                        @svg('fad-sun', 'h-5 w-5')
+                        <span class="ml-2">Light</span>
+                    </div>
+                </flux:radio>
+                <flux:radio value="dark">
+                    <div class="flex items-center">
+                        @svg('fad-moon', 'h-5 w-5')
+                        <span class="ml-2">Dark</span>
+                    </div>
+                </flux:radio>
+                <flux:radio value="system">
+                    <div class="flex items-center">
+                        @svg('fad-computer', 'h-5 w-5')
+                        <span class="ml-2">System</span>
+                    </div>
+                </flux:radio>
+            </flux:radio.group>
+        </div>
         <flux:tab.panel name="fping">
             @livewire('fping::fping-preferences')
         </flux:tab.panel>
